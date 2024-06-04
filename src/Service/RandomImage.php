4108934946271dd0a5fa39c1b3f1412a5b5e7913
SergeyShobuhin <shobuhin@mail.ru>
2024-06-04 11:17:15 +0700

    fix

--- src/Service/RandomImage.php
+++ src/Service/RandomImage.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 class RandomImage
 {
+    //Access Key=uUdatS6483NfCP16P0lek_xfxyBNSVkX_n5dzyJFFKs
     private const API_URL = 'https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY';
     public function __construct(private readonly string $key)
     {
